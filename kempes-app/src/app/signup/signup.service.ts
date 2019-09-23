7726e9e3eb2c8b69be75fc412b8058eaa7cab09b
jaglinsub <jaglinsub@gmail.com>
2019-09-22 22:40:47 -0500

    first commit

---
+++ kempes-app/src/app/signup/signup.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export class PersonTypes {
+  id: number;
+  personType: string;
+}
+export const PERSONTYPES: PersonTypes[] = [
+  {
+    "id": 1,
+    personType: "Student"
+  },
+  {
+    "id": 2,
+    personType: "Councillor"
+  },
+  {
+    "id": 3,
+    personType: "Parent"
+  }
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  constructor() { }
+
+  getPersonTypes(): Observable<PersonTypes[]> {
+    return of(PERSONTYPES);
+  }
+}
+
